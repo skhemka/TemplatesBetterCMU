4abea3c3676d89efcd45395393be5315447827a5
Shreeyagya Khemka <skhemka@andrew.cmu.edu>
2016-09-28 14:41:52 -0400

    Followed and Post page

---
+++ js/click.js
@@ -0,0 +1,9 @@
+var follow = 1500;
+var like = 1000;
+var dislike = 200;
+
+$("#follow").click( function() {
+    console.log("follow button clicked");
+    follow+=1;
+    console.log(follow);
+});
